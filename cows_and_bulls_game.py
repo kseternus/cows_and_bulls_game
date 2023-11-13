8ebca27c061e392446c7f9714b8348caf68154c4
Kamil Seternus <60841895+kseternus@users.noreply.github.com>
2023-11-13 17:10:20 +0100

    Add files via upload

---
+++ cows_and_bulls_game.py
@@ -0,0 +1,39 @@
+import random
+
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+number = []
+guess = ''
+tries = 0
+
+random.shuffle(numbers)
+
+def random_number(number):
+    number +=numbers.pop(0)
+
+def cows_bulls():
+    bulls = 0
+    cows = 0
+    for i in range(4):
+        if guess[i] == number[i]:
+            bulls += 1
+        elif guess[i] in number:
+            cows += 1
+    print(f'Bulls: {bulls}, cows {cows}')
+
+def main():
+    global guess, tries
+    print('Welcome to Cows&Bulls game!')
+    while guess != number:
+        guess = str(input('Enter 4 digit number: '))
+        if len(guess) != 4:
+            print('4 digits please! ')
+        else:
+            cows_bulls()
+            tries += 1
+    print(f'You are correct! number was {number}! You guessed it after {tries} tries!')
+
+for _ in range(4):
+    random_number(number)
+number = "".join(dict.fromkeys(number))
+
+main()
